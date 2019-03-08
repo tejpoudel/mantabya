ecdb668c637f16e8d44480cc296d2e57269df8e1
Tej Poudel <newsbinodp@gmail.com>
2019-03-07 22:11:18 -0800

    Added User Schema

--- server.js
+++ server.js
@@ -5,16 +5,30 @@ const bodyParser = require('body-parser');
 const app = express();
 
 //Bodyparser Middleware 
+app.use(bodyParser.urlencoded({ extended:false}));
 app.use(bodyParser.json());
 
+// Map global promise - get rid of warning
+mongoose.Promise = global.Promise;
+
 //DB Config
-const db = require('./config/key').mongoURI;
+//const db = require('./config/key').mongoURI;
 
 //Connect to Mongo 
-mongoose
-.connect(db)
-.then(() => console.log('MongoDB Connected.....'))
+// mongoose
+// .connect(db)
+// .then(() => console.log('MongoDB Connected.....'))
+// .catch(err => console.log(err));
+
+// Connect to Mongoose
+mongoose.connect('mongodb://localhost/Mantabya-DB')
+.then(() => console.log('MongoDB Connected....'))
 .catch(err => console.log(err));
+
+//Index Route
+app.get("/", (req, res) => {
+    res.render('index');
+});
 
 
 const port = process.env.PORT || 5001;
